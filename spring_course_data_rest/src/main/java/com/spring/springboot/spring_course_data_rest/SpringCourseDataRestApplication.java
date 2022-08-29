54cb8e1c4e8e5dc90275c2c25d51f33642983a5c
upskill2 <6105_7@ukr.net>
2022-08-29 22:03:14 +0300

    first commit

---
+++ spring_course_data_rest/src/main/java/com/spring/springboot/spring_course_data_rest/SpringCourseDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springboot.spring_course_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseDataRestApplication.class, args);
+	}
+
+}
---
+++ spring_course_data_rest/src/test/java/com/spring/springboot/spring_course_data_rest/SpringCourseDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springboot.spring_course_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
